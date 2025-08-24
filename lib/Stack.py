f9987e9c28969a479945fcdd7f03ea7d05831f30
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2025-08-24 00:12:32 -0400

    Passes all tests

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,34 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
+
+    def size(self):
+        return len(self.items)
 
     def isEmpty(self):
-        pass
-
-    def push(self, item):
-        pass
-
-    def pop(self):
-        pass
-
-    def peek(self):
-        pass
-    
-    def size(self):
-        pass
+        return len(self.items) == 0
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
+
+    def push(self, item):
+        if not self.full():
+            self.items.append(item)
+
+    def pop(self):
+        if not self.isEmpty():
+            return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
 
     def search(self, target):
-        pass
+        length = self.size() - 1
+        for i in range(len(self.items)):
+            if target == self.items[i]:
+                return length
+            elif target != self.items[i]:
+                length -= 1
+        return -1   
